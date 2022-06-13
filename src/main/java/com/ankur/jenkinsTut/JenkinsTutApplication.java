3ff2fc62546f2b7545e0cb3162a5dddbebf7da60
ankurmanna <ankurma@zeta.tech>
2022-06-13 11:00:06 +0530

    JenkinsTut Test

---
+++ src/main/java/com/ankur/jenkinsTut/JenkinsTutApplication.java
@@ -0,0 +1,13 @@
+package com.ankur.jenkinsTut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankur/jenkinsTut/JenkinsTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankur.jenkinsTut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
